aac5d45dce5e27ad55a4db4f7f9ab6a97e0f8dec
Ronald Yin <zidyin_jx@126.com>
2025-04-21 21:12:14 -0400

    added summary word limit

--- news_agent/gpt_summary.py
+++ news_agent/gpt_summary.py
@@ -9,7 +9,9 @@ def summarize_financial_news(text,api_key):
     # client = OpenAI(api_key=os.environ.get(api_key))
     client = OpenAI(api_key=api_key)
     prompt = f"""
-        The following is an economic news article. Please summarize the essence of the article in plain language and briefly analyze its possible impact on the market:
+        The following is an economic news article. 
+        Please summarize the essence of the article in plain language and briefly analyze its possible impact on the market
+        (within 50 words):
         """ + text
     response = client.chat.completions.create(
             model="gpt-4o",
@@ -21,7 +23,7 @@ def summarize_financial_news(text,api_key):
     return response.choices[0].message.content.strip()
 
 # testing
-# if __name__ == "__main__":
-#     text = "UnitedHealth Group stock had one of its worst days ever on Thursday after the healthcare giant unexpectedly cut its profit forecast for the year."
-#     summary = summarize_financial_news(text,api_key)
-#     print(summary)
+if __name__ == "__main__":
+    text = "UnitedHealth Group stock had one of its worst days ever on Thursday after the healthcare giant unexpectedly cut its profit forecast for the year."
+    summary = summarize_financial_news(text,api_key)
+    print(summary)
