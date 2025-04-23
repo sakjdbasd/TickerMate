02799f08ee7ae6c5a4a3e7495e6bf53cd63da75c
Ronald Yin <zidyin_jx@126.com>
2025-04-23 11:20:55 -0400

    decorate with rest_framework

--- tickermate_backend/news_agent/views.py
+++ tickermate_backend/news_agent/views.py
@@ -3,21 +3,28 @@ import os
 from dotenv import load_dotenv
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 load_dotenv()
 
+@api_view(["GET"])
 def summarized_news_api(request):
     ticker = request.GET.get("ticker", "").upper()
     if not ticker:
-        return JsonResponse({"error": "Missing ticker symbol"}, status=400)
+        # return JsonResponse({"error": "Missing ticker symbol"}, status=400)
+        return Response({"error": "Missing ticker symbol"}, status=status.HTTP_400_BAD_REQUEST)
 
     news_api_key = os.getenv("NEWS_API_KEY")
     openai_api_key = os.getenv("OPENAI_API_KEY")
 
     try:
         data = get_summarized_news(ticker, news_api_key, openai_api_key)
-        return JsonResponse(data, safe=False, status=200)
+        # return JsonResponse(data, safe=False, status=200)
+        return Response(data, status=status.HTTP_200_OK)
     except Exception as e:
-        return JsonResponse({"error": str(e)}, status=500)
+        # return JsonResponse({"error": str(e)}, status=500)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
--- tickermate_backend/tickermate_backend/urls.py
+++ tickermate_backend/tickermate_backend/urls.py
@@ -15,9 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="TickerMate API",
+      default_version='v1',
+      description="API documentation for financial summarizer agent",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("news_agent.urls")),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui()),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
